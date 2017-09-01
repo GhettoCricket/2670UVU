041f67563dc9b49b4c8d49f6e5383dd2484b7412
deggleston <10671954@my.uvu.edu>
2017-09-01 16:14:23 -0600

    Added Jumping

--- Scripting2670/Assets/Scripts/MoveInput.cs
+++ Scripting2670/Assets/Scripts/MoveInput.cs
@@ -5,8 +5,13 @@ using System;
 public class MoveInput : MonoBehaviour {
 
 	public static Action<float> KeyAction;
+	public static Action JumpAction;
 
 	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			JumpAction();
+		}
 		if (KeyAction != null)
 		{
 			KeyAction(Input.GetAxis("Horizontal"));
