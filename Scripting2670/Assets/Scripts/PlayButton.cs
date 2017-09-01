09ad664b61fb36c3dbea5be65f7703ce97de281a
deggleston <10671954@my.uvu.edu>
2017-09-01 13:34:46 -0600

    Attempted to Make scripts

---
+++ Scripting2670/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayButton : MonoBehaviour {
+
+	public static Action Play; 
+
+	public void PushPlay() {
+	Play();
+	}
+}
+
