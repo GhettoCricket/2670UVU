8cc98d089e319454a65d35f1c0c623906f445589
deggleston <10671954@my.uvu.edu>
2017-10-02 11:40:20 -0600

    Added Throwing

--- GamePrototype/Assets/Scripts/AttachtoPlayer.cs
+++ GamePrototype/Assets/Scripts/AttachtoPlayer.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class AttachtoPlayer : MonoBehaviour {
 
 	Transform attachObject;
+	Rigidbody _Rigidbody;
+	public Vector3 ThrowV;
+	bool isAttached = false;
+	bool inAir = true;
 
 	void Awake()
 	{
 		SendAttach.SendAttachPoint += AttachPointHandler;
+		_Rigidbody = GetComponent<Rigidbody>();
 
 		
 	}
@@ -19,11 +24,43 @@ public class AttachtoPlayer : MonoBehaviour {
 
 	void OnTriggerStay(Collider other)
 	{
-		if(Input.GetKey(KeyCode.E) && other.tag == "Player")
+		if(Input.GetKeyDown(KeyCode.E) && other.tag == "Player" && isAttached == false)
 		{
-		transform.parent = attachObject;
-		transform.localPosition = Vector3.zero;
-		transform.localRotation = Quaternion.identity;
+			_Rigidbody.isKinematic = true;
+			transform.parent = attachObject;
+			transform.localPosition = Vector3.zero;
+			transform.localRotation = Quaternion.identity;
+			isAttached = true;
 		}
+		if (Input.GetKeyDown(KeyCode.R) && isAttached == true)
+		{
+			transform.parent = null;
+			_Rigidbody.isKinematic = false;
+			isAttached = false;
+			inAir = true;
+			StartCoroutine(ThrowDistance());
+			
+			print("Throw");
+		}
+		
 	}
+	IEnumerator ThrowDistance()
+		{	print("Start Throw");
+			ThrowV.x = .2f;
+			ThrowV.y = .1f;
+			while(inAir && isAttached == false)
+			{
+				ThrowV.x -= 0.1f*Time.deltaTime;
+				ThrowV.y -= 0.1f*Time.deltaTime;
+				transform.Translate(ThrowV);
+				print(ThrowV);
+				if(ThrowV.x <= 0f)
+				{
+					inAir = false;
+				}
+				yield return new WaitForSeconds(0.01f);
+			}
+
+			
+		}
 }
