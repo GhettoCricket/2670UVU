cd1ff7f8f9f475f2ab65291686c90fc3b6f1896c
deggleston <10671954@my.uvu.edu>
2017-11-14 11:40:46 -0700

    Worked on the Blend Trees

--- GameFinal/Assets/Scripts/Old Scripts/AnimController.cs
+++ GameFinal/Assets/Scripts/Old Scripts/AnimController.cs
@@ -8,18 +8,18 @@ public class AnimController : MonoBehaviour {
 	// Use this for initialization
 
 	public static Animator animator;
-	CharacterController cc;
 	private float Movefloat_X;
 	private float Movefloat_Y;
 	void Start () {
 		animator = GetComponent<Animator>();
-		cc = GetComponent<CharacterController>();
 		MoveInput.KeyAction += Animate;
 	}
 	
 	private void Animate(float obj)
 	{	
-		Movefloat_Y = cc.velocity.y;
+		Movefloat_Y = MoveCharacter.tempMove.y;
+		if(Movefloat_Y < -1)
+			Movefloat_Y = -1;
 		Movefloat_X = Mathf.Abs(MoveCharacter.tempMove.x*7);
 		animator.SetFloat("Velocity_X", Movefloat_X);
 		animator.SetFloat("Velocity_Y", Movefloat_Y);
